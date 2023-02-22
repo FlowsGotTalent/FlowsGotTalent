c5b1352c2c60b71f650c808ec4ed425c661ad02d
Dan O <diydan82@gmail.com>
2023-02-22 18:30:50 +0100

    Added First mini game: Scissors Paper Rock

---
+++ ui/src/pages/games/base.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+  <VRow class="match-height">
+    <VCol
+      cols="12"
+      md="4"
+      class="text-center mx-auto"
+    >
+      <v-chip size="large" color="primary">Round 1</v-chip>
+      <h2>
+        Scissors Paper Rock
+      </h2>
+      <p>Best of 3</p>
+    </VCol>
+  </VRow>
+  <VRow class="match-height">
+    <VCol
+      cols="12"
+      md="4"
+      class="text-center mx-auto"
+    >
+      <v-chip size="large" color="primary">Round 1</v-chip>
+      <h2>
+        Scissors Paper Rock
+      </h2>
+      <p>Best of 3</p>
+    </VCol>
+  </VRow>
+</template>
